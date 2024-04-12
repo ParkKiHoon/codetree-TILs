d67b879cc0e6aadffe296b351a2c4bb440b53c1c
KIHOON PARK <vpfmtlsl@naver.com>
2024-04-12 21:58:35 +0900

    Title: 루돌프의 반란, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240412/루돌프의 반란/rudolph-rebellion.py
+++ 240412/루돌프의 반란/rudolph-rebellion.py
@@ -2,19 +2,15 @@ n, m, p, c, d = map(int,input().split())
 rr, rc = map(int,input().split())
 rr-=1
 rc-=1
-santa = []
+santa = [[]for _ in range(p)]
 board=[[-1 for _ in range(n)] for _ in range(n)]
 stun=[0 for i in range(p)]
 for i in range(p):
     tmp=list(map(int,input().split()))
-    santa.append([tmp[1]-1,tmp[2]-1,0,1])
-    board[tmp[1]-1][tmp[2]-1]=i
+    santa[tmp[0]-1]=[tmp[1]-1,tmp[2]-1,0,1]
+    board[tmp[1]-1][tmp[2]-1]=tmp[0]-1
 
-for i in range(n):
-    print(board[i])
-print(santa)
-print(rr,rc)
-print("")
+
 def distance(r1,c1,r2,c2):
     return (r1-r2)**2 + (c1-c2)**2
 
@@ -52,7 +48,7 @@ def move_dolph_rule(r1,c1,r2,c2):
     return r1,c1
 
 def dolph_interaction(dr,dc,r2,c2,cnt):
-    if cnt == c: return
+
     r22=r2+dr
     c22=c2+dc
     ind = board[r2][c2]
@@ -65,16 +61,16 @@ def dolph_interaction(dr,dc,r2,c2,cnt):
     else:
         santa[ind][3]=0
 def move_dolph_collide(dr,dc,r2,c2,cnt):
-    if cnt==c: return
-    r22=r2+dr
-    c22=c2+dc
+
+    r22=r2+dr*c
+    c22=c2+dc*c
     ind = board[r2][c2]
     santa[ind] = [r22, c22, santa[ind][2]+c, santa[ind][3]]
     board[r2][c2] = -1
     if 0<=r22<n and 0<=c22<n:
         if board[r22][c22] != -1:
             dolph_interaction(dr,dc,r22,c22,cnt+1)
-        print(r2,c2,dr,dc,r22,c22,"A")
+
         board[r22][c22]=ind
         stun[ind]=2
     else:
@@ -83,8 +79,8 @@ def move_dolph_collide(dr,dc,r2,c2,cnt):
 def move_santa(r1,c1,rr,rc):
     santa_pos=[-1,-1]
     minval=9999999999
-    dx=[-1,0,1,0]
-    dy=[0,1,0,-1]
+    dx=[0,1,0,-1]
+    dy=[-1,0,1,0]
     for i in range(4):
         x=r1+dx[i]
         y=c1+dy[i]
@@ -98,8 +94,7 @@ def move_santa(r1,c1,rr,rc):
     return santa_pos
 
 def santa_interaction(dr,dc,r2,c2,cnt):
-    if cnt == p:
-        return
+
     r22=r2+dr
     c22=c2+dc
     ind = board[r2][c2]
@@ -112,10 +107,10 @@ def santa_interaction(dr,dc,r2,c2,cnt):
     else:
         santa[ind][3]=0
 def move_santa_collide(dr,dc,r2,c2,cnt):
-    if cnt==p:
-        return
-    r22=r2+dr
-    c22=c2+dc
+
+    r22=r2+dr*(d-1)
+    c22=c2+dc*(d-1)
+
     ind = board[r2][c2]
     santa[ind] = [r22, c22, santa[ind][2] + d, santa[ind][3]]
     board[r2][c2] = -1
@@ -127,7 +122,7 @@ def move_santa_collide(dr,dc,r2,c2,cnt):
     else:
         santa[ind][3]=0
 
-for times in range(6):
+for times in range(m):
     r2,c2=move_dolph(rr,rc)
     r1,c1=move_dolph_rule(rr,rc,r2,c2)
     dr, dc = r1 - rr, c1 - rc
@@ -145,7 +140,6 @@ for times in range(6):
                 if [x,y]==[rr,rc]:
                     move_santa_collide(santa[i][0]-x,santa[i][1]-y,santa[i][0],santa[i][1],0)
                 else:
-                    print("AB")
                     board[santa[i][0]][santa[i][1]] = -1
                     santa[i] = [x, y, santa[i][2], santa[i][3]]
                     board[x][y]=i
@@ -157,10 +151,16 @@ for times in range(6):
         if santa[i][3]==1:
             santa[i][2]+=1
 
-    for i in range(n):
-        print(board[i])
 
-    print(santa)
-    print(rr,rc)
-    print(stun)
-    print("")
+    exitP=1
+    for i in range(p):
+        if santa[i][3]==1:
+            exitP=0
+    if exitP==1:
+        break
+
+for i in range(p):
+    if i==p-1:
+        print(santa[i][2],end="")
+    else:
+        print(santa[i][2],end=" ")
